4a12a47fd47622257a79d3920c167b3cfd2dc978
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2016-08-18 08:54:08 +0900

    add annotation resoinse data' key

--- src/main/java/net/sokontokoro_factory/himono/controller/form/CreateDeviceForm.java
+++ src/main/java/net/sokontokoro_factory/himono/controller/form/CreateDeviceForm.java
@@ -1,21 +1,31 @@
 package net.sokontokoro_factory.himono.controller.form;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlValue;
 
 @XmlRootElement
 public class CreateDeviceForm {
+    @NotBlank
+    @JsonProperty("device_id")
     @Getter
     @Setter
     private String id;
 
+    @NotBlank
+    @JsonProperty("device_name")
     @Getter
     @Setter
     private String name;
 
+    @NotBlank
+    @JsonProperty("manage_user_id")
     @Getter
     @Setter
     private String userId;
--- src/main/java/net/sokontokoro_factory/himono/controller/form/CreateUserForm.java
+++ src/main/java/net/sokontokoro_factory/himono/controller/form/CreateUserForm.java
@@ -1,26 +1,35 @@
 package net.sokontokoro_factory.himono.controller.form;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class CreateUserForm {
     @NotBlank(message = "UserID is required.")
+    @JsonProperty("user_id")
     @Getter
     @Setter
     private String id;
 
     @NotBlank(message = "Username is required.")
+    @JsonProperty("user_name")
     @Getter
     @Setter
     private String name;
 
     @NotBlank(message = "Addreess is required.")
+    @JsonProperty("address")
     @Getter
     @Setter
     private String address;
 
     @NotBlank(message = "Password is required.")
+    @JsonProperty("password")
     @Getter
     @Setter
     private String password;
